ad681bf8db55a2f280e0ffb92b4b491894953e2c
Chris Ibarra <chrisibarra818@outlook.com>
2025-11-05 18:39:17 -0800

    Add files via upload

---
+++ Practica_Listas-Playlist/Practica_Listas-Playlist/Playlist.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Listas_Playlist
+{
+    public class  Playlist
+    {
+        public List<string> canciones = new List<string>();
+        public void AgregarCancion(string cancion)
+        {
+            Console.Write("Escriba el nombre de la cancion a agregar: ");
+            string nuevaCancion = Console.ReadLine();
+            canciones.Add(nuevaCancion);
+            Console.WriteLine($"Cancion '{nuevaCancion}' agregada a la playlist.");
+        }
+        public void InsertarCancion(string cancion, int posicion)
+        {
+            Console.Write("Escriba el nombre de la cancion a insertar: ");
+            string cancionInsertar = Console.ReadLine();
+            Console.Write("Escriba la posicion donde desea insertar la cancion: ");
+            int posicionInsertar = int.Parse(Console.ReadLine());
+            canciones.Insert(posicionInsertar, cancionInsertar);
+            Console.WriteLine($"Cancion '{cancionInsertar}' insertada en la posicion {posicionInsertar}.");
+        }
+        public void EliminarCancion(string cancion)
+        {
+            Console.Write("Escriba el nombre de la cancion a eliminar: ");
+            string cancionEliminar = Console.ReadLine();
+            if (canciones.Remove(cancionEliminar))
+            {
+                Console.WriteLine($"Cancion '{cancionEliminar}' eliminada de la playlist.");
+            }
+            else
+            {
+                Console.WriteLine($"La cancion '{cancionEliminar}' no se encontro en la playlist.");
+            }
+        }
+        public void MostrarCanciones()
+        {
+            Console.WriteLine("Canciones en la playlist:");
+            foreach (var cancion in canciones)
+            {
+                Console.WriteLine("- " + cancion);
+            }
+        }
+
+    }
+}
---
+++ Practica_Listas-Playlist/Practica_Listas-Playlist/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Practica_Listas_Playlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Playlist miPlaylist = new Playlist();
+            bool salir = false;
+            while (!salir)
+            {
+                Console.WriteLine("\nMenu de la Playlist:");
+                Console.WriteLine("1. Agregar cancion");
+                Console.WriteLine("2. Insertar cancion en posicion");
+                Console.WriteLine("3. Eliminar cancion");
+                Console.WriteLine("4. Mostrar canciones");
+                Console.WriteLine("5. Salir");
+                Console.Write("Seleccione una opcion (1-5): ");
+                string opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        miPlaylist.AgregarCancion("");
+                        break;
+                    case "2":
+                        miPlaylist.InsertarCancion("", 0);
+                        break;
+                    case "3":
+                        miPlaylist.EliminarCancion("");
+                        break;
+                    case "4":
+                        miPlaylist.MostrarCanciones();
+                        break;
+                    case "5":
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida. Por favor, intente de nuevo.");
+                        break;
+                }
+            }
+        }
+    }
+}
